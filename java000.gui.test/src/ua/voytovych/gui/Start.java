6ad7d21d0c67e7f242c3875f7260cf91bf808ce6
Ivan Voytovych <voytovych.ua@gmail.com>
2016-05-20 14:54:57 +0300

    First commit

---
+++ java000.gui.test/src/ua/voytovych/gui/Start.java
@@ -0,0 +1,15 @@
+package ua.voytovych.gui;
+
+import javax.swing.JFrame;
+
+public class Start {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+		
+	}
+
+}
