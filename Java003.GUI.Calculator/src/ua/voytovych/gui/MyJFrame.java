790c668a129010059860b9380adb143d42f50c03
Ivan Voytovych <voytovych.ua@gmail.com>
2016-05-23 14:53:35 +0300

    Change skin

--- Java003.GUI.Calculator/src/ua/voytovych/gui/MyJFrame.java
+++ Java003.GUI.Calculator/src/ua/voytovych/gui/MyJFrame.java
@@ -2,11 +2,14 @@ package ua.voytovych.gui;
 
 import java.awt.Component;
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.LayoutManager;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MyJFrame extends JFrame {
+	
+	private Image icon = new ImageIcon(getClass().getResource("/images/icon.png")).getImage();
 
 	public MyJFrame(String title, int width, int height) {
 		super(title);
@@ -14,7 +17,7 @@ public class MyJFrame extends JFrame {
 		super.setLocationRelativeTo(null);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		super.setLayout(new FlowLayout());
-		super.setIconImage(new ImageIcon("icon.png").getImage());
+		super.setIconImage(icon);
 	}
 
 	public MyJFrame(String title, int width, int height, Component comp) {
